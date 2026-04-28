f18bec9c89ac65bad1be3c88504b28be9ef6f4ba
Gustavo <277551575+GMarchiote@users.noreply.github.com>
2026-04-28 23:04:46 +0000

    Desafio xadrez nível aventureiro concluído

--- xadrez.c
+++ xadrez.c
@@ -3,25 +3,42 @@
 int main()
 {
     // Mover a peça torre 5 casas para a direita
-    int torre = 1, bispo = 1;
+    int torre = 1, bispo = 1, cavalo = 1;
     while (torre <= 5)
     {
         printf("Torre: direita\n");
         torre++;
     }
-    printf("---- // ----\n");
+
+    printf("\n");
+
     do // Mover a peça bispo 5 casas na diagonal para cima e à direita
     {
         printf("Bispo: cima\n");
         printf("Bispo: direita\n");
         bispo++;
     } while (bispo <= 5);
-    printf("---- // ----\n");
+
+    printf("\n");
 
     for (int rainha = 1; rainha <= 8; rainha++) // Mover a peça rainha oito casas para a esquerda
     {
         printf("Rainha: esquerda\n"); 
     }
-    
+
+    printf("\n");
+    printf("Cavalo: ");
+    for (cavalo = 1; cavalo <= 1;) // Mover a peça do cavalo duas vezes para baixo e uma para esquerda
+    {
+        do
+        {
+            printf("baixo, ");
+            cavalo++;
+        } while (cavalo <= 2);
+
+        printf("esquerda\n");
+        
+    }
+      
     return 0;
 }
