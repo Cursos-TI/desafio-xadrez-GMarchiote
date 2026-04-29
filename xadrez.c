b5e7c4456b0186c9e6b99b4f723b13284f4435e4
Gustavo <277551575+GMarchiote@users.noreply.github.com>
2026-04-29 21:56:46 +0000

    Desafio nível mestre concluído

--- xadrez.c
+++ xadrez.c
@@ -1,44 +1,69 @@
 #include <stdio.h>
 
-int main()
-{
-    // Mover a peça torre 5 casas para a direita
-    int torre = 1, bispo = 1, cavalo = 1;
-    while (torre <= 5)
+void torre(int t){ // controla a movimentação da torre
+    if (t > 0)
     {
-        printf("Torre: direita\n");
-        torre++;
-    }
+        printf("direita\n");
+        torre (t - 1);
+    }    
+}
 
-    printf("\n");
-
-    do // Mover a peça bispo 5 casas na diagonal para cima e à direita
-    {
-        printf("Bispo: cima\n");
-        printf("Bispo: direita\n");
-        bispo++;
-    } while (bispo <= 5);
-
-    printf("\n");
-
-    for (int rainha = 1; rainha <= 8; rainha++) // Mover a peça rainha oito casas para a esquerda
-    {
-        printf("Rainha: esquerda\n"); 
-    }
-
-    printf("\n");
-    printf("Cavalo: ");
-    for (cavalo = 1; cavalo <= 1;) // Mover a peça do cavalo duas vezes para baixo e uma para esquerda
-    {
-        do
+void bispo(int b){ // controla a movimentação do bispo
+    if (b > 0) {
+        int i, d;
+        for (i = 1; i < 2; i++)
         {
-            printf("baixo, ");
-            cavalo++;
-        } while (cavalo <= 2);
+            printf("cima\n");
+            for (d = 1; d < 2; d++)
+            {
+                printf("direita\n");
+            }
+        }
+        bispo(b - 1);
+    } 
+}
 
+void rainha(int r){ // controla a movimentação da rainha
+    if (r > 0)
+    {
         printf("esquerda\n");
-        
-    }
-      
+        rainha (r - 1);
+    }  
+}
+
+void cavalo(int c){ // controla a movimentação do cavalo
+    if (c > 0){
+        for (int i = 1; i <= 2; i++) {
+            printf("Cima\n");
+                for (int j = 1; j <= 1 && i == 2; j++) {
+                printf("Direita\n");
+                }
+            }
+        cavalo(c - 1);
+        }
+}
+
+int main ()
+{
+    int t = 5, b = 5, r = 8, c = 1;
+    // Parte do código responsável por invocar as recursividades e imprimir as movimentações das devidas peças
+    printf("Movimentação da torre: \n");
+    torre(t);
+
+    printf("\n");
+
+    printf("Movimentação do bispo: \n");
+    bispo(b);
+
+    printf("\n");
+
+    printf("Movimentação da rainha: \n");
+    rainha (r);
+
+    printf("\n");
+
+    printf("Movimentação do cavalo: \n");
+    cavalo(c);    
+    
     return 0;
 }
